d175d76b955e22cef38f95743184ccf1b8fd0d4a
ammann64 <austinmmann64@gmail.com>
2023-10-23 14:57:02 -0500

    Rework file structure and add basic server routes

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const noteData = require('./db/db.json');
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) =>  {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Note taking app listening at http://localhost:${PORT}`);
+  });
+  
