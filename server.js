915532b86fd6a1bac30dcb59b5548a6637ef9a68
ammann64 <austinmmann64@gmail.com>
2023-10-24 13:22:21 -0500

    Fix port for heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const uniqid = require('uniqid');
 const noteData = require('./db/db.json');
 const dbPath = './db/db.json';
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 const app = express();
 
